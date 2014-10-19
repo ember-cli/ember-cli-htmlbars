c9aab0429cb34cc456a5b15bc843cdb32f97be9d
Robert Jackson <robert.w.jackson@me.com>
2014-10-18 20:16:29 -0400

    Clear registry for hbs files.

--- ember-addon-main.js
+++ ember-addon-main.js
@@ -8,6 +8,10 @@ module.exports = {
     var HTMLBars = app.project.config(app.env).EmberENV.FEATURES.HTMLBars
 
     this._super.included.apply(this, arguments);
+
+    // ensure that broccoli-ember-hbs-template-compiler is not processing hbs files
+    app.registry.remove('template', 'broccoli-ember-hbs-template-compiler');
+
     app.registry.add('template', {
       name: 'ember-cli-htmlbars',
       ext: 'hbs',
