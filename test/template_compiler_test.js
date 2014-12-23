fbbf13943d63383db16ec55a4642f492a684f8d2
Robert Jackson <robert.w.jackson@me.com>
2014-12-23 16:25:04 -0500

    Use ember-template-compiler paired with Ember.

--- test/template_compiler_test.js
+++ test/template_compiler_test.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var broccoli = require('broccoli');
 var assert = require('assert');
 var templateCompilerFilter = require('../index');
-var htmlbarsCompiler = require('htmlbars').compileSpec;
 var handlbarsTemplateCompiler = require('ember-template-compiler');
 
 var builder;
@@ -19,35 +18,46 @@ describe('templateCompilerFilter', function(){
   });
 
   describe('HTMLBars', function() {
-    var htmlbarsOptions;
+    var htmlbarsOptions, htmlbarsPrecompile;
 
     beforeEach(function() {
       htmlbarsOptions = {
-        disableComponentGeneration: true
+        isHTMLBars: true,
+        templateCompiler: require('../bower_components/ember/ember-template-compiler')
       };
+
+      htmlbarsPrecompile = htmlbarsOptions.templateCompiler.precompile;
+    });
+
+    afterEach(function() {
+
     });
 
     it('precompiles templates into htmlbars', function(){
-      var tree = templateCompilerFilter(sourcePath, { htmlbarsOptions: htmlbarsOptions });
+      var tree = templateCompilerFilter(sourcePath, htmlbarsOptions);
 
       builder = new broccoli.Builder(tree);
       return builder.build().then(function(results) {
         var actual = fs.readFileSync(results.directory + '/template.js', { encoding: 'utf8'});
         var source = fs.readFileSync(sourcePath + '/template.hbs', { encoding: 'utf8' });
-        var expected = "export default Ember.HTMLBars.template(" + htmlbarsCompiler(source) + ");";
+        var expected = "export default Ember.HTMLBars.template(" + htmlbarsPrecompile(source) + ");";
 
         assert.equal(actual,expected,'They dont match!')
       });
     });
 
-    it('passes provided options to htmlbars', function(){
-      var tree = templateCompilerFilter(sourcePath, { htmlbarsOptions: htmlbarsOptions });
+    it('passes FEATURES to compiler', function(){
+      htmlbarsOptions.FEATURES = {
+        'ember-htmlbars-component-generation': true
+      };
+
+      var tree = templateCompilerFilter(sourcePath, htmlbarsOptions);
 
       builder = new broccoli.Builder(tree);
       return builder.build().then(function(results) {
         var actual = fs.readFileSync(results.directory + '/web-component-template.js', { encoding: 'utf8'});
         var source = fs.readFileSync(sourcePath + '/web-component-template.hbs', { encoding: 'utf8' });
-        var expected = "export default Ember.HTMLBars.template(" + htmlbarsCompiler(source, htmlbarsOptions) + ");";
+        var expected = "export default Ember.HTMLBars.template(" + htmlbarsPrecompile(source) + ");";
 
         assert.equal(actual,expected,'They dont match!')
       });
@@ -62,7 +72,7 @@ describe('templateCompilerFilter', function(){
       return builder.build().then(function(results) {
         var actual = fs.readFileSync(results.directory + '/non-standard-extension.js', { encoding: 'utf8'});
         var source = fs.readFileSync(sourcePath + '/non-standard-extension.handlebars', { encoding: 'utf8' });
-        var expected = 'export default Ember.Handlebars.template(' + handlbarsTemplateCompiler.precompile(source, false) + ')';
+        var expected = 'export default Ember.Handlebars.template(' + handlbarsTemplateCompiler.precompile(source, false) + ');';
 
         assert.equal(actual,expected,'They dont match!')
       });
@@ -71,7 +81,7 @@ describe('templateCompilerFilter', function(){
     function assertOutput(results) {
       var actual = fs.readFileSync(results.directory + '/template.js', { encoding: 'utf8'});
       var source = fs.readFileSync(sourcePath + '/template.hbs', { encoding: 'utf8' });
-      var expected = 'export default Ember.Handlebars.template(' + handlbarsTemplateCompiler.precompile(source, false) + ')';
+      var expected = 'export default Ember.Handlebars.template(' + handlbarsTemplateCompiler.precompile(source, false) + ');';
 
       assert.equal(actual,expected,'They dont match!')
     }
