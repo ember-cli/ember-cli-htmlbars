dbe5bf17e856b3f33be370f79ce695a4b9949530
Stefan Penner <stefan.penner@gmail.com>
2016-02-29 08:53:29 -0800

    ensure '.js' extension if no extension was provided

--- ember-addon-main.js
+++ ember-addon-main.js
@@ -67,7 +67,13 @@ module.exports = {
       templateCompilerPath = this.project.bowerDirectory + '/ember/ember-template-compiler';
     }
 
-    return path.resolve(this.project.root, templateCompilerPath);
+    var absolutePath = path.resolve(this.project.root, templateCompilerPath);
+
+    if (path.extname(absolutePath) === '') {
+      absolutePath += '.js';
+    }
+
+    return absolutePath;
   },
 
   htmlbarsOptions: function() {
