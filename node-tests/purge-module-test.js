c60f6c39bf227b3f80004e0bf3596c20373e4872
Robert Jackson <rjackson@linkedin.com>
2019-09-08 16:16:13 -0400

    Move code into `lib/` subdirectory.

--- node-tests/purge-module-test.js
+++ node-tests/purge-module-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const purgeModule = require('../ember-addon-main').purgeModule;
+const purgeModule = require('../lib/ember-addon-main').purgeModule;
 const expect = require('chai').expect;
 
 describe('purgeModule', function() {
