270d6351eecf183e5dbc693c04febf71ed0ece9f
Chris Eppstein <chris@eppsteins.net>
2020-04-20 13:48:36 -0700

    Move template compiler creation to a method on the addon so it can be overridden by plugins (e.g. CSS Blocks) that need access to the input and output trees.

---
+++ lib/template-compiler-plugin.d.ts
@@ -0,0 +1,15 @@
+import type Plugin from "broccoli-plugin";
+import type { ASTPluginBuilder } from "@glimmer/syntax";
+import type { InputNode } from "broccoli-node-api";
+type PluginOptions = ConstructorParameters<typeof Plugin>[1]; // this should be exported by the main import.
+export declare interface HtmlBarsOptions extends PluginOptions {
+  plugins?: {
+    ast?: Array<ASTPluginBuilder>
+  };
+}
+declare class TemplateCompiler extends Plugin {
+  public extensions: Array<string>;
+  public targetExtension: string;
+  constructor(inputTree: InputNode, options: HtmlBarsOptions);
+}
+export = TemplateCompiler;
