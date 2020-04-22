ee16a5c7c5b6066e77cd07de9e1f73473da44354
Chris Eppstein <chris@eppsteins.net>
2020-04-22 13:25:12 -0700

    Fix error in type definition file.

--- lib/template-compiler-plugin.d.ts
+++ lib/template-compiler-plugin.d.ts
@@ -2,14 +2,16 @@ import type Plugin from "broccoli-plugin";
 import type { ASTPluginBuilder } from "@glimmer/syntax";
 import type { InputNode } from "broccoli-node-api";
 type PluginOptions = ConstructorParameters<typeof Plugin>[1]; // this should be exported by the main import.
-export declare interface HtmlBarsOptions extends PluginOptions {
-  plugins?: {
-    ast?: Array<ASTPluginBuilder>
-  };
+declare namespace TemplateCompiler {
+  interface HtmlBarsOptions extends PluginOptions {
+    plugins?: {
+      ast?: Array<ASTPluginBuilder>
+    };
+  }
 }
 declare class TemplateCompiler extends Plugin {
   public extensions: Array<string>;
   public targetExtension: string;
-  constructor(inputTree: InputNode, options: HtmlBarsOptions);
+  constructor(inputTree: InputNode, options: TemplateCompiler.HtmlBarsOptions);
 }
 export = TemplateCompiler;
