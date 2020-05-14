8a8a6163e9c699758540912370e7cd96683f8bfa
Chris Eppstein <chris@eppsteins.net>
2020-05-14 14:07:53 -0700

    Upgrade to broccoli-persistent-filter version 3.0.

--- lib/template-compiler-plugin.d.ts
+++ lib/template-compiler-plugin.d.ts
@@ -1,7 +1,9 @@
-import type Plugin from "broccoli-plugin";
-import type { ASTPluginBuilder } from "@glimmer/syntax";
-import type { InputNode } from "broccoli-node-api";
-type PluginOptions = ConstructorParameters<typeof Plugin>[1]; // this should be exported by the main import.
+import Filter = require('broccoli-persistent-filter');
+import type { ASTPluginBuilder } from '@glimmer/syntax';
+import type { InputNode } from 'broccoli-node-api';
+// This is a hack to get access to the abstract Filter parameters which aren't exported.
+declare class Foo extends Filter { }
+type PluginOptions = ConstructorParameters<typeof Foo>[1]; // this should be exported by the main import.
 declare namespace TemplateCompiler {
   interface HtmlBarsOptions extends PluginOptions {
     plugins?: {
@@ -9,9 +11,13 @@ declare namespace TemplateCompiler {
     };
   }
 }
-declare class TemplateCompiler extends Plugin {
-  public extensions: Array<string>;
-  public targetExtension: string;
+declare class TemplateCompiler extends Filter {
+  public extensions: ['hbs', 'handlebars'];
+  public targetExtension: 'js';
   constructor(inputTree: InputNode, options: TemplateCompiler.HtmlBarsOptions);
+  registeredASTPlugins(): Array<ASTPluginBuilder>;
+  unregisterPlugins(): void;
+  baseDir(): string;
+  optionsHash(): string;
 }
 export = TemplateCompiler;
