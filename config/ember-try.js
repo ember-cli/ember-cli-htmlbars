9b2c9eab4ec7e37edb3cd8a71defdf5871f6f89d
Edward Faulkner <ef@alum.mit.edu>
2021-10-14 02:34:26 -0400

    dropping embroider-optimized
    Because the shenanigans with the custom mergeTrees here:
    
    ```js
    appTree = new MergeTree(['tests/dummy/app', 'tests/colocation/app']);
    ```
    
    Breaks staticComponents.
    
    And also, I don't think this addon is particularly important to test under embroider-optimized anyway.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
+const { embroiderSafe } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
@@ -84,7 +84,6 @@ module.exports = async function () {
         },
       },
       embroiderSafe(),
-      embroiderOptimized(),
       {
         name: 'with-ember-cli-htmlbars-inline-precompile',
         npm: {
