a95a4fedc7a06dc077cd986d426e87205f351e08
Edward Faulkner <edward@eaf4.com>
2022-06-30 01:01:39 -0400

    Drive all template compilation from babel
    Classically, standalone templates got a dedicated preprocessor that had nothing to do with Javascript transpilation.
    
    Later came inline templates that appear inside Javascript. Those were handled as a totally separate code path from the standalone templates. To this day, there are two different code paths for handling these two cases.
    
    But at this point, templates-inside-javascript are the foundational primitive that is aligned with [where Ember is heading](https://github.com/emberjs/rfcs/pull/779), because they have access to Javascript scope and this solves a lot of problems.
    
    We can treat standalone HBS as just a degenerate kind of JS that is easily up-compiled via a pure function, allowing them to go down the Javascript code path and allowing us to drop all the old code path.
    
    This also makes it easier to support new features like [AST transforms that can manipulate Javascript scope](https://github.com/emberjs/babel-plugin-ember-template-compilation/pull/5).
    
    Embroider already [implemented this same pattern](https://github.com/embroider-build/embroider/pull/1010).

--- node-tests/template_compiler_test.js
+++ node-tests/template_compiler_test.js
@@ -2,355 +2,42 @@
 
 const assert = require('assert');
 const TemplateCompiler = require('../lib/template-compiler-plugin');
-const co = require('co');
 const { createTempDir, createBuilder } = require('broccoli-test-helper');
 const fixturify = require('fixturify');
-const { stripIndent } = require('common-tags');
+const jsStringEscape = require('js-string-escape');
 
 describe('TemplateCompiler', function () {
   this.timeout(10000);
 
   let input, output, builder;
 
-  beforeEach(
-    co.wrap(function* () {
-      input = yield createTempDir();
-      input.write(fixturify.readSync(`${__dirname}/fixtures`));
-    })
-  );
-
-  afterEach(
-    co.wrap(function* () {
-      if (builder) {
-        builder.cleanup();
-      }
-
-      yield input.dispose();
-
-      if (output) {
-        yield output.dispose();
-      }
-    })
-  );
-
-  let htmlbarsOptions, htmlbarsPrecompile;
-
-  beforeEach(function () {
-    htmlbarsOptions = {
-      isHTMLBars: true,
-      plugins: {
-        ast: [],
-      },
-      templateCompiler: require('ember-source/dist/ember-template-compiler.js'),
-      templateCompilerPath: require.resolve('ember-source/dist/ember-template-compiler.js'),
-    };
-
-    htmlbarsPrecompile = htmlbarsOptions.templateCompiler.precompile;
+  beforeEach(async function () {
+    input = await createTempDir();
+    input.write(fixturify.readSync(`${__dirname}/fixtures`));
   });
 
-  describe('requiresModuleApiPolyfill: true', function () {
-    it(
-      'precompiles templates into htmlbars',
-      co.wrap(function* () {
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, true);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('template.hbs');
-        let expected = `export default Ember.HTMLBars.template(${htmlbarsPrecompile(source, {
-          moduleName: 'template.hbs',
-        })});`;
-        assert.strictEqual(output.readText('template.js'), expected);
-      })
-    );
-
-    it('invokes AST plugins', async function () {
-      let source = '{{foo-bar}}';
-      input.write({
-        'template.hbs': source,
-      });
-      let plugin = (env) => {
-        return {
-          name: 'fake-ast-plugin',
-
-          visitor: {
-            MustacheStatement() {
-              return env.syntax.builders.text('Huzzah!');
-            },
-          },
-        };
-      };
-
-      htmlbarsOptions.plugins.ast.push(plugin);
-
-      let tree = new TemplateCompiler(input.path(), htmlbarsOptions, true);
-
-      output = createBuilder(tree);
-      await output.build();
-
-      let expected = `export default Ember.HTMLBars.template(${htmlbarsPrecompile(source, {
-        moduleName: 'template.hbs',
-        plugins: {
-          ast: [plugin],
-        },
-      })});`;
-
-      let outputString = output.readText('template.js');
-      assert.strictEqual(outputString, expected);
-      assert.ok(outputString.includes('Huzzah!'));
-    });
-
-    it('AST Plugins have access to `isProduction` status', async function () {
-      let source = '{{foo-bar}}';
-      input.write({
-        'template.hbs': source,
-      });
-
-      let wasProduction = false;
-      let plugin = (env) => {
-        wasProduction = env.isProduction;
-
-        return {
-          name: 'fake-ast-plugin',
-
-          visitor: {},
-        };
-      };
-
-      htmlbarsOptions.isProduction = true;
-      htmlbarsOptions.plugins.ast.push(plugin);
-
-      let tree = new TemplateCompiler(input.path(), htmlbarsOptions, true);
-
-      output = createBuilder(tree);
-      await output.build();
-
-      assert.ok(wasProduction);
-    });
-
-    it(
-      'ignores utf-8 byte order marks',
-      co.wrap(function* () {
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, true);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('template.hbs');
-        let expected = `export default Ember.HTMLBars.template(${htmlbarsPrecompile(source, {
-          moduleName: 'template-with-bom.hbs',
-        })});`;
-
-        assert.strictEqual(output.readText('template-with-bom.js'), expected);
-      })
-    );
-
-    it(
-      'passes FEATURES to compiler when provided as `FEATURES` [DEPRECATED]',
-      co.wrap(function* () {
-        htmlbarsOptions.FEATURES = {
-          'ember-htmlbars-component-generation': true,
-        };
-
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, true);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('web-component-template.hbs');
-        let expected = `export default Ember.HTMLBars.template(${htmlbarsPrecompile(source, {
-          moduleName: 'web-component-template.hbs',
-        })});`;
-
-        assert.strictEqual(output.readText('web-component-template.js'), expected);
-      })
-    );
-
-    it(
-      'passes FEATURES to compiler when provided as `EmberENV.FEATURES`',
-      co.wrap(function* () {
-        htmlbarsOptions.EmberENV = {
-          FEATURES: {
-            'ember-htmlbars-component-generation': true,
-          },
-        };
-
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, true);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('web-component-template.hbs');
-        let expected = `export default Ember.HTMLBars.template(${htmlbarsPrecompile(source, {
-          moduleName: 'web-component-template.hbs',
-        })});`;
-
-        assert.strictEqual(output.readText('web-component-template.js'), expected);
-      })
-    );
+  afterEach(async function () {
+    if (builder) {
+      builder.cleanup();
+    }
+    await input.dispose();
+    if (output) {
+      await output.dispose();
+    }
   });
 
-  describe('requiresModuleApiPolyfill: false', function () {
-    it(
-      'precompiles templates into htmlbars',
-      co.wrap(function* () {
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, false);
+  it('converts hbs into JS', async function () {
+    let tree = new TemplateCompiler(input.path());
 
-        output = createBuilder(tree);
-        yield output.build();
+    output = createBuilder(tree);
+    await output.build();
 
-        let source = input.readText('template.hbs');
-        let expected = stripIndent`
-          import { createTemplateFactory } from '@ember/template-factory';
-
-          export default createTemplateFactory(${htmlbarsPrecompile(source, {
-            moduleName: 'template.hbs',
-          })});
-        `;
-        assert.strictEqual(output.readText('template.js'), expected);
-      })
-    );
-
-    it('invokes AST plugins', async function () {
-      let source = '{{foo-bar}}';
-      input.write({
-        'template.hbs': source,
-      });
-      let plugin = (env) => {
-        return {
-          name: 'fake-ast-plugin',
-
-          visitor: {
-            MustacheStatement() {
-              return env.syntax.builders.text('Huzzah!');
-            },
-          },
-        };
-      };
-
-      htmlbarsOptions.plugins.ast.push(plugin);
-
-      let tree = new TemplateCompiler(input.path(), htmlbarsOptions, false);
-
-      output = createBuilder(tree);
-      await output.build();
-
-      let expected = stripIndent`
-        import { createTemplateFactory } from '@ember/template-factory';
-
-        export default createTemplateFactory(${htmlbarsPrecompile(source, {
-          moduleName: 'template.hbs',
-          plugins: {
-            ast: [plugin],
-          },
-        })});
-      `;
-
-      let outputString = output.readText('template.js');
-      assert.strictEqual(outputString, expected);
-      assert.ok(outputString.includes('Huzzah!'));
-    });
-
-    it('AST Plugins have access to `isProduction` status', async function () {
-      let source = '{{foo-bar}}';
-      input.write({
-        'template.hbs': source,
-      });
-
-      let wasProduction = false;
-      let plugin = (env) => {
-        wasProduction = env.isProduction;
-
-        return {
-          name: 'fake-ast-plugin',
-
-          visitor: {},
-        };
-      };
-
-      htmlbarsOptions.isProduction = true;
-      htmlbarsOptions.plugins.ast.push(plugin);
-
-      let tree = new TemplateCompiler(input.path(), htmlbarsOptions, false);
-
-      output = createBuilder(tree);
-      await output.build();
-
-      assert.ok(wasProduction);
-    });
-
-    it(
-      'ignores utf-8 byte order marks',
-      co.wrap(function* () {
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, false);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('template.hbs');
-        let expected = stripIndent`
-          import { createTemplateFactory } from '@ember/template-factory';
-
-          export default createTemplateFactory(${htmlbarsPrecompile(source, {
-            moduleName: 'template-with-bom.hbs',
-          })});
-        `;
-
-        assert.strictEqual(output.readText('template-with-bom.js'), expected);
-      })
-    );
-
-    it(
-      'passes FEATURES to compiler when provided as `FEATURES` [DEPRECATED]',
-      co.wrap(function* () {
-        htmlbarsOptions.FEATURES = {
-          'ember-htmlbars-component-generation': true,
-        };
-
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, false);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('web-component-template.hbs');
-        let expected = stripIndent`
-          import { createTemplateFactory } from '@ember/template-factory';
-
-          export default createTemplateFactory(${htmlbarsPrecompile(source, {
-            moduleName: 'web-component-template.hbs',
-          })});
-        `;
-
-        assert.strictEqual(output.readText('web-component-template.js'), expected);
-      })
-    );
-
-    it(
-      'passes FEATURES to compiler when provided as `EmberENV.FEATURES`',
-      co.wrap(function* () {
-        htmlbarsOptions.EmberENV = {
-          FEATURES: {
-            'ember-htmlbars-component-generation': true,
-          },
-        };
-
-        let tree = new TemplateCompiler(input.path(), htmlbarsOptions, false);
-
-        output = createBuilder(tree);
-        yield output.build();
-
-        let source = input.readText('web-component-template.hbs');
-        let expected = stripIndent`
-          import { createTemplateFactory } from '@ember/template-factory';
-
-          export default createTemplateFactory(${htmlbarsPrecompile(source, {
-            moduleName: 'web-component-template.hbs',
-          })});
-        `;
-
-        assert.strictEqual(output.readText('web-component-template.js'), expected);
-      })
-    );
+    let source = input.readText('template.hbs');
+    let expected = [
+      `import { hbs } from 'ember-cli-htmlbars';`,
+      `export default hbs('${jsStringEscape(source)}', { moduleName: 'template.hbs' });`,
+      '',
+    ].join('\n');
+    assert.strictEqual(output.readText('template.js'), expected);
   });
 });
