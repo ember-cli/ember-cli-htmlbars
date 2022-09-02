a8eebbe89ca57a76fe9c469a11ca3750f18ac2a6
Chris Manson <chris@manson.ie>
2022-09-02 11:39:01 +0100

    add a test to cover addon templates

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,8 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL,
 });
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('styleguide');
+});
 
 export default Router;
